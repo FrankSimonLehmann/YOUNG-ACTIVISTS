8bc6ccb2787dad97e851a9204e6131ea26026139
Usman Ali <usmanv5@hotmail.com>
2023-12-05 15:10:33 +0000

    some progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @demonstrations = Demonstration.all
+  end
+
+  def user
+    @user = User.find(current_user.id)
   end
 end
