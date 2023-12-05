db438f59aec75476b57d57d03706c20fb9429f2a
Usman Ali <usmanv5@hotmail.com>
2023-12-05 13:52:42 +0000

    profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :demonstrations, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
