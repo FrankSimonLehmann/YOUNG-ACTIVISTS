68055c794741290e4f35b91e527987fe8ea2b140
Frank Lehmann <franksimonlehmann@hotmail.com>
2023-12-05 12:28:02 +0100

    created the pundit setup

--- app/controllers/demonstrations_controller.rb
+++ app/controllers/demonstrations_controller.rb
@@ -1,2 +1,8 @@
 class DemonstrationsController < ApplicationController
+
+  def show
+    @demonstration = Demonstration.find(params[:id])
+    authorize @demonstration # Add this line
+  end
+
 end
