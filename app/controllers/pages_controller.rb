652db64dff27a5b965379acfce199fa719dab2e1
Usman Ali <usmanv5@hotmail.com>
2023-12-05 15:37:54 +0000

    upcoming demo logic done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @demo = []
     @demonstrations = Demonstration.all
+    @demonstrations.each do |demonstration|
+      if demonstration.start_time > (Time.now)
+        @demo << demonstration
+      end
+    end
+    @demo = @demo.sort_by { |demo| demo.start_time}
   end
 
   def user
