b7516f8cbb0c909c42a788cc77000887f167d631
theresa schottl <theresaschoettl1@gmail.com>
2023-12-12 14:46:44 +0100

    changes to nav

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,7 +20,21 @@ export default class extends Controller {
     this.#fitMapToMarkers()
   }
 
-   git push          .addTo(this.map)
+    #fitMapToMarkers(){
+      const bounds = new mapboxgl.LngLatBounds()
+      this.markersValue.forEach((marker) => {
+        bounds.extend([ marker.lng, marker.lat]);
+      });
+      this.map.fitBounds(bounds, {padding: 50, duration: 10, maxZoom: 15})
+    }
+
+
+    #addMarkersToMap() {
+      this.markersValue.forEach((marker) => {
+        new mapboxgl.Marker()
+          // .setLngLat([ marker.lat, marker.lng ])
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(this.map)
     })
   }
 }
