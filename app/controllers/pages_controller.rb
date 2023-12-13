ba919c0c2147fdb54b1ed4c6dc0ce4d4b5c86e56
Usman Ali <usmanv5@hotmail.com>
2023-12-13 15:59:46 +0000

    profile page stuff ascending order fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,6 +47,9 @@ class PagesController < ApplicationController
   def profile
     @user = User.find(current_user.id)
     @bookmark = Bookmark.where(user_id: current_user.id)
+    @bookmark = @bookmark.sort_by { |bookmark| bookmark.demonstration.start_time}
+    @demonstrations = @user.demonstrations
+    @demonstrations = @demonstrations.sort_by { |demonstration| demonstration.start_time}
   end
 
   def bookmarked
